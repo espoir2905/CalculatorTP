4d900109b724984b5c70b45b714ecb008f982a7e
HEllou Sabrina <hellou.sabrina>
2019-11-28 11:32:29 +0100

    ignorer des dossiers et giter les autres sur lesquels j'ai travaillé

---
+++ CalculatorUnitTest/CalculatorEngineUnitTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorUnitTest
+{
+    [TestClass]
+    public class CalculatorEngineUnitTest
+    {
+        [TestMethod]
+        public void TestMethodadd()
+        {
+            int a = 1;
+            int b = 2;
+            int exp = 3;
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.add(a, b);
+            Assert.AreEqual(exp, actual);
+          
+            
+        }
+        [TestMethod]
+        public void TestMethodSub()
+        {
+            int a = 5;
+            int b = 10;
+            int sub = -5;
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.sub(a, b);
+            Assert.AreEqual(sub, actual);
+        }
+        [TestMethod]
+        public void TestMethodMul()
+        {
+            int a = 5;
+            int b = 10;
+            int mul = 50;
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.Mul(a, b);
+            Assert.AreEqual(mul, actual);
+        }
+        [TestMethod]
+        public void TestMethodDiv()
+        {
+            int a = 10;
+            int b = 10;
+            int div = 1;
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.Div(a, b);
+            Assert.AreEqual(div, actual);
+        }
+        [TestMethod]
+        public void TestMethodDivByZero()
+        {
+            int a = 6;
+            int b = 0;
+            int div = int.MinValue;
+            CalculatorEngine engine = new CalculatorEngine();
+            int actual = engine.Div(a, b);
+            Assert.AreEqual(div, actual);
+        }
+
+    }
+}
---
+++ Console/CalculatorEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+    public class CalculatorEngine
+    {
+        public int add(int a,int b)
+        {
+            return a+b;
+        }
+       public int sub(int a, int b)
+        {
+            return a-b;
+        }
+       public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int val1, int val2)
+        {
+            int res;
+            if (val2 != 0)
+            {
+                res = val1 / val2;
+            }
+            else
+            {
+                res = Int32.MinValue;
+            }
+            return res;
+        }
+    }
+}
